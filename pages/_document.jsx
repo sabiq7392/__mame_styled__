9368787e8a3be5dc9ef9758630464e5e7ed8d00c
Sabiq Muhammad Antebing Mame <72848275+sabiq7392@users.noreply.github.com>
2022-05-03 18:43:57 +0700

    add globalstyled, remove globalscss

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,9 @@ export default function Document() {
   
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300&display=swap" rel="stylesheet" />
+      </Head>
       <body style={{ background: color.base.primary }}>
         <Main />
         <NextScript />
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+    /* margin: 0;
+    padding: 0; */
+  }
+
+  h1, h2, h3, h4, h5, h6, p, figure, blockquote {
+    margin: 0!important;
+  }
+`;

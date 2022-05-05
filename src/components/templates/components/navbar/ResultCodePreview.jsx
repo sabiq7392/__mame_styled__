93abd9a14c12c26f7854c910965df1239bf7330c
Sabiq Muhammad Antebing Mame <72848275+sabiq7392@users.noreply.github.com>
2022-05-06 04:09:59 +0700

    navbar dynamic import

--- src/components/templates/components/navbar/ResultCodePreview.jsx
+++ src/components/templates/components/navbar/ResultCodePreview.jsx
@@ -1,4 +1,5 @@
-import CodePreview from "../../../organisms/CodePreview";
+import dynamic from "next/dynamic";
+const CodePreview = dynamic(() => import("../../../organisms/CodePreview"));
 
 export default function ResultCodePreview() {
   return <>
--- src/components/templates/components/navbar/ResultPreview.jsx
+++ src/components/templates/components/navbar/ResultPreview.jsx
@@ -1,5 +1,8 @@
 import Preview from "../../../organisms/Preview";
-import MameNavbar from "./_MameNavbar";
+import dynamic from "next/dynamic";
+
+const MameNavbar = dynamic(() => import("./_MameNavbar"));
+// import MameNavbar from "./_MameNavbar";
 
 export default function ResultPreview() {
   return <>

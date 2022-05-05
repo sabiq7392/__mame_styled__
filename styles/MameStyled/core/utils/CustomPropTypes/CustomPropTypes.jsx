59ab46453627d0eb5f27133a0679cd9a6f6e75c8
Sabiq Muhammad Antebing Mame <72848275+sabiq7392@users.noreply.github.com>
2022-05-05 09:03:26 +0700

    add error canot combined props

--- styles/MameStyled/core/utils/CustomPropTypes/CustomPropTypes.jsx
+++ styles/MameStyled/core/utils/CustomPropTypes/CustomPropTypes.jsx
@@ -1,23 +1,23 @@
-export function requiredIf(
-  { 
-    notExistProps, 
-    shouldType,
-    isRequired,
-  }, 
-  props, 
-  propName, 
-  // eslint-disable-next-line no-unused-vars
-  componentName, 
-) {
-  Object.keys(props).forEach((key) => {
-    notExistProps.forEach((_notExistProp) => {
-      if (key === _notExistProp) {
-        console.error(`Cannot combined with props: ${_notExistProp}`);
-      }
-    });
-  });
+// export function requiredIf(
+//   { 
+//     notExistProps, 
+//     shouldType,
+//     isRequired,
+//   }, 
+//   props, 
+//   propName, 
+//   // eslint-disable-next-line no-unused-vars
+//   componentName, 
+// ) {
+//   Object.keys(props).forEach((key) => {
+//     notExistProps.forEach((_notExistProp) => {
+//       if (key === _notExistProp) {
+//         console.error(`Cannot combined with props: ${_notExistProp}`);
+//       }
+//     });
+//   });
 
-  if (typeof props[propName] !== shouldType) {
-    console.error(`Must be ${shouldType}`);
-  }
-}
+//   if (typeof props[propName] !== shouldType) {
+//     console.error(`Must be ${shouldType}`);
+//   }
+// }

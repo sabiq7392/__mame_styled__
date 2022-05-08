1f1e18bbfa71a24916553c612ee4a3947dd5e24c
Sabiq Muhammad Antebing Mame <72848275+sabiq7392@users.noreply.github.com>
2022-05-08 07:13:42 +0700

    fix bug image carouse

--- src/components/templates/components/carousel/_MameCarousel.jsx
+++ src/components/templates/components/carousel/_MameCarousel.jsx
@@ -1,15 +1,88 @@
 import { Div, H2, P, Button, Img, Figure, Figcaption } from "../../../../../styles/MameStyled/core/HtmlTag";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useCallback } from "react";
 
 export default function MameCarousel() {
   const carouselContainer = useRef();
   const [carouselItems, setCarouselItems] = useState([]);
   const [isActive, setIsActive] = useState(0);
 
+  const repositionCarouselItemsWhenResize = useCallback(() => {
+    const observer = new ResizeObserver(entries => {
+      const entry = entries[0].target.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+
+      return entry;
+    });
+
+    observer.observe(document.getElementsByClassName("mame-carousel-item")[isActive]);
+  }, [isActive]);
+
   useEffect(() => {
     setCarouselItems([...carouselContainer.current.children]);
-  }, []);
+    repositionCarouselItemsWhenResize();
+  }, [repositionCarouselItemsWhenResize]);
 
+  return <>
+    <Carousel>
+      <CarouselItemsContainer _ref={carouselContainer}>
+        <CarouselItem>
+          <Figure>
+            <Img 
+              src="/images/photos/example-1.jpg"
+              alt="example-1"
+              style={{ width: "100%", height: 300, objectFit: "cover" }}
+            />
+            <Figcaption cssXs={{ color: "#00B4D8" }}>
+              <H2>Example 1</H2>
+              <P cssXs={{ color: "#90E0EF" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem delectus voluptatum explicabo deleniti! 
+                Alias dolores illum ad rem dicta magni sit temporibus ullam nam, corporis distinctio officiis sapiente voluptatum facere?
+              </P>
+            </Figcaption>
+          </Figure>
+        </CarouselItem>
+        <CarouselItem>
+          <Figure>
+            <Img 
+              src="/images/photos/example-2.jpg"
+              alt="example-1"
+              style={{ width: "100%", height: 300, objectFit: "cover" }}
+            />
+            <Figcaption cssXs={{ color: "#00B4D8" }}>
+              <H2>Example 2</H2>
+              <P cssXs={{ color: "#90E0EF" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem delectus voluptatum explicabo deleniti! 
+                Alias dolores illum ad officiis sapiente voluptatum facere?
+              </P>
+            </Figcaption>
+          </Figure>
+        </CarouselItem>
+        <CarouselItem>
+          <Figure>
+            <Img 
+              src="/images/photos/example-3.jpg"
+              alt="example-1"
+              style={{ width: "100%", height: 300, objectFit: "cover" }}
+            />
+            <Figcaption cssXs={{ color: "#00B4D8" }}>
+              <H2>Example 3</H2>
+              <P cssXs={{ color: "#90E0EF" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. voluptatum explicabo deleniti! 
+                Alias dolores illum ad officiis sapiente voluptatum facere? 
+              </P>
+            </Figcaption>
+          </Figure>
+        </CarouselItem>
+      </CarouselItemsContainer>
+      <CarouselIndicatorButtons 
+        carouselItems={carouselItems}
+        isActive={isActive}
+        setIsActive={setIsActive}
+      />
+    </Carousel>
+  </>;
+}
+
+function Carousel({ children }) {
   return <>
     <Div
       cssXs={{ 
@@ -17,74 +90,72 @@ export default function MameCarousel() {
         gap: "1rem"
       }}
     >
-      <Div 
-        ref={carouselContainer}
-        cssXs={{ 
-          display: "flex",
-          background: "#222",
-          width: "100%",
-          // height: 400,
-          overflowX: "hidden",
-        }}
-      >
-        <CarouselItem>
-          <Figure>
-            <Img 
-              src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=426&w=640"
-              alt="example-1"
-              style={{ width: "100%", height: 300, objectFit: "cover" }}
-            />
-            <Figcaption cssXs={{ color: "#00B4D8" }}>
-              <H2>Documentation</H2>
-              <P cssXs={{ color: "#90E0EF" }}>Find in-depth information about Next.js features and API.</P>
-            </Figcaption>
-          </Figure>
-        </CarouselItem>
-        <CarouselItem>
-          <H2 cssXs={{ color: "#00B4D8" }}>Deploy</H2>
-          <P cssXs={{ color: "#90E0EF" }}>Instantly deploy your Next.js site to a public URL with Vercel.</P>
-        </CarouselItem>
-      </Div>
-      <Div cssXs={{ display: "flex", gap: 3 }}>
-        {carouselItems.map((carouselItem, index) => 
-          <Button
-            key={index}
-            cssXs={{
-              width: 16,
-              aspectRatio: "1 / 1",
-              background: isActive === index ? "#00B4D8" : "rgba(0,0,0,0)",
-              border: "2px solid #00B4D8",
-              borderRadius: "50%",
-              cursor: "pointer",
-            }} 
-            onClick={() => {
-              setIsActive(index);
-              carouselItem.scrollIntoView({ behavior: "smooth", block: "center" });
-            }}
-          />  
-        )}
-      </Div>
+      {children}
+    </Div>
+  </>;
+}
+
+function CarouselItemsContainer({ children, _ref }) {
+  return <>
+    <Div 
+      ref={_ref}
+      cssXs={{ 
+        display: "flex",
+        background: "#222",
+        width: "100%",
+        // height: 400,
+        overflowX: "hidden",
+      }}
+    >
+      {children}
+    </Div>
+  </>;
+}
+
+function CarouselIndicatorButtons({ carouselItems, isActive, setIsActive }) {
+  return <>
+    <Div cssXs={{ display: "flex", gap: 3 }}>
+      {carouselItems.map((carouselItem, index) => 
+        <Button
+          key={index}
+          cssXs={{
+            width: 16,
+            aspectRatio: "1 / 1",
+            background: isActive === index ? "#00B4D8" : "rgba(0,0,0,0)",
+            border: "2px solid #00B4D8",
+            borderRadius: "50%",
+            cursor: "pointer",
+          }} 
+          onClick={() => {
+            setIsActive(index);
+            carouselItem.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+          }}
+        />  
+      )}
     </Div>
   </>;
 }
 
 function CarouselItem({ children }) {
+  
+  useEffect(() => {
+
+  }, []);
   return <>
     <Div 
-      // id={title.toLowerCase()}
       className="mame-carousel-item"
       cssXs={{ 
         display: "flex",
-        flexDirection: "column", 
         flexShrink: 0,
-        // width: "inherit",
         width: "100%",
         gap: 10, 
         height: "100%", 
         padding: "2rem" 
       }}
     >
-      {children}
+      <Div>
+        {children}
+      </Div>
     </Div>
   </>;
 }
